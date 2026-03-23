5d9982a092aa9c2888075a2546849ea8b2904903
Ayush Gupta <ayushgupta.silversky@gmail.com>
2026-03-23 18:58:28 +0530

    blocking user from the macOS

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
 import { PersonService } from './person.service';
 import { Person } from './dto/person.entity';
 import { CreatePersonDto } from './dto/create-person-dto';
+import { agent } from 'supertest';
 import { ApiException } from 'src/Errors/ApiException';
 
 @Controller('person')
@@ -20,11 +21,19 @@ export class PersonController {
 
   @Get(':id')
   findSingleById(@Req() req, @Param('id') id: string) {
-    console.log(req);
-    console.log(req.headers.accept);
+    const userAgent = req.headers['user-agent'].toLowerCase().split(' ');
+    console.log(userAgent);
+    //This will block the incomming request from the mac os
+    /*
+    if (userAgent.includes('mac')) {
+        throw new ApiException("You can't allow here Fuck off", 500);
+    }
+    */
+
     // If we use this then we have to send accept mannual on FE
     // if (req.headers.accept !== 'application/json')
     //   throw new ApiException(`can't work with headers accept`, 500);
+
     return this.personService.findSinglePerson(Number(id));
   }
 }
