740d53ad23dcbe823b0488eb795017b1d384a73a
Ayush Gupta <ayushgupta.silversky@gmail.com>
2026-03-24 11:34:11 +0530

    API on Query parameter

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
 import { PersonService } from './person.service';
 import { Person } from './dto/person.entity';
 import { CreatePersonDto } from './dto/create-person-dto';
@@ -22,14 +22,21 @@ export class PersonController {
 
   @Post('/dummy')
   dummyPost(@Body() data) {
+    // This show undefine
+    // Check main.ts
     console.log(data);
+  }
+
+  // Here order matters
+  // If I move this function after :id, then :id will be invoked, and it won’t get a chance to run, so keep this in mind.
+  @Get('/short')
+  findSinglePerson(@Query('name') name: string, @Query('id') id: number) {
+    console.log(name, id);
+    return 'Found';
   }
 
   @Get(':id')
   findSingleById(@Req() req, @Param('id') id: string) {
-    console.log(req.route);
-    const userAgent = req.headers['user-agent'].toLowerCase().split(' ');
-    console.log(userAgent);
     //This will block the incomming request from the mac os
     /*
     if (userAgent.includes('mac')) {
